39b584cfdda775483018ebe0c5bc3a11d01a5541
baidala <stavr@bk.ru>
2021-02-24 12:27:12 +0200

    init

---
+++ app/src/main/java/ua/com/citrusdev/broadcasttest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.com.citrusdev.broadcasttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
